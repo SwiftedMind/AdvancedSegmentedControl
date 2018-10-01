a4a6b6e567d66ee4d58c12be545819fa60045966
Dennis Müller <d3mueller@me.com>
2018-10-01 11:36:34 +0200

    Initial Commit

---
+++ AdvancedSegmentedControl/AdvancedSegmentedControl.h
@@ -0,0 +1,19 @@
+//
+//  AdvancedSegmentedControl.h
+//  AdvancedSegmentedControl
+//
+//  Created by Dennis Müller on 01.10.18.
+//  Copyright © 2018 Dennis Müller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdvancedSegmentedControl.
+FOUNDATION_EXPORT double AdvancedSegmentedControlVersionNumber;
+
+//! Project version string for AdvancedSegmentedControl.
+FOUNDATION_EXPORT const unsigned char AdvancedSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdvancedSegmentedControl/PublicHeader.h>
+
+
